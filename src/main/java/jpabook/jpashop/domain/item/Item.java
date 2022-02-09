4707472e9aa137cf4d785ebebd460928e4567fc7
Jisu Han <weblite@naver.com>
2022-02-09 18:10:34 +0900

    상품 엔티티에 비즈니스 로직을 추가 + NotEnoughStockException 예외를 직접 만듦

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.item;
 
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,7 +13,7 @@ import java.util.List;
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)  // 한테이블에 모두 속성을 넣는다. +@Inheritance 는 DB의 슈퍼타입, 서브타입 관계를 매핑
 @DiscriminatorColumn(name="dtype")  // 부모 클래스에 선언한다. 하위 클래스를 구분하는 용도의 컬럼
-@Getter @Setter
+@Getter
 public abstract class Item {
 
     @Id @GeneratedValue
@@ -26,6 +27,18 @@ public abstract class Item {
     @ManyToMany(mappedBy = "items")
     private List<Category> categories= new ArrayList<>();
 
+    /* 비즈니스 로직의 추가 */
+    // Item.addStock 하면 재고수량이 늘고, Item.removeStock하면 재고수량이 줄도록 설계
+    public void addStock(int quantity){
+        this.stockQuantity+=quantity;
+    }
 
+    public void removeStock(int quantity){
+        int restStock=this.stockQuantity-quantity;
+        if(restStock<0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity=restStock;
+    }
 
 }
---
+++ src/main/java/jpabook/jpashop/exception/NotEnoughStockException.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop.exception;
+
+
+// 예외를 만들어봄. 수량 부족 예외를 발생시키도록
+public class NotEnoughStockException extends RuntimeException{
+    public NotEnoughStockException() {
+        super();
+    }
+
+    public NotEnoughStockException(String message) {
+        super(message);
+    }
+
+    public NotEnoughStockException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotEnoughStockException(Throwable cause) {
+        super(cause);
+    }
+
+    protected NotEnoughStockException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
