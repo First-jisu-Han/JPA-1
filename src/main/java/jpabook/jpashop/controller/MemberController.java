aeaea53218b948a89e208b2a4b829e2fca2d002c
Jisu Han <weblite@naver.com>
2022-02-15 02:21:30 +0900

    웹계층 : 회원등록 컨트롤러 +회원목록 조회  (엔티티를 최대한 순수하게 , 컨트롤러등 화면을 위한 로직은 안됨 DTO나 데이터를 전송하기 위한 객체를 사용하도록한다. )

---
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -0,0 +1,55 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String createForm(Model model){
+        model.addAttribute("memberForm",new MemberForm());
+        return "members/createMemberForm";
+    }
+    // 폼을 전송한 이후 매커니즘 - DTO 와 비슷한 개념으로 설계된듯
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm form, BindingResult result){
+
+        // BindingResult를 통해서 뭔가를 입력을 덜해서 에러가 나면 멤버 폼으로 다시 가게한다.+ 빨간줄,@NotEmpty로 적용한 메세지 적용
+        if(result.hasErrors()){
+            return "members/createMemberForm";
+        }
+
+        Address address=new Address(form.getCity(),form.getStreet(),form.getZipcode());
+
+        Member member=new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members=memberService.findMembers();
+        model.addAttribute("members",members);
+        return "members/memberList";
+    }
+
+
+
+}
